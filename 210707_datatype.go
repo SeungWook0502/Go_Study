69e7374f9cb44f4000e9abf827161b7da1e3e1d0
SeungWook0502 <SeungWook0502@gmail.com>
2021-07-08 09:14:30 +0900

    day1

---
+++ 210707_datatype.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+
+//bool
+//string -> 생성시 수정될 수 없다.
+//int int8 int16 int32 int64
+//uint uint8 uint16 uint32 uint64 uintptr
+//float32 float64 complex64 complex128
+//byte == unit8 -> byte-code
+//rune == int32 -> uni-code
+
+func main(){
+	rawLiteral := `아리랑\n아리랑` //raw string literal
+	interLiteral := "아리랑\n아라리요" //interpreted string literal
+
+	fmt.Println(rawLiteral+"\n")
+	fmt.Println(interLiteral)
+
+//Type Conversion
+	i := 100
+	u := uint64(i)
+	f := float32(u)
+	fmt.Println(f,u)
+
+	s := "ABC"
+	b := []byte(s)
+	s2 := string(b)
+	fmt.Println(b,s2)
+}
---
+++ 210707_variable.go
@@ -0,0 +1,17 @@
+//annotation
+var a int //var -> variable
+var f float32 = 11 // '=' -> initialize
+var s string = ""
+var bool bool = true
+// z := 23 // var z = 23 Must be in func | Short Assignment Statement
+
+a = 10 //non semi-colon
+//b = 10//error -> non declare this variable
+var b,c,d int = 1,2,3 //variable multiple declare
+const e int = 10 //constants
+const f = 10 // non declare datatype
+const(
+	Apple = iota //0
+	Grape
+	Orange
+)
